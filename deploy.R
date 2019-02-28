b3f9151915eba3aeff300bae7459e08505b16a2d
Harrison McGehee <harrison.mcgehee@unos.org>
2019-02-28 08:19:48 -0500

    fix pull

--- deploy.R
+++ deploy.R
@@ -1,10 +1,4 @@
-<<<<<<< HEAD
 
-plumber::plumb(dir = getwd())$run()
-
-
-debugonce(plumber::plumb)
-=======
 library(plumber)
 plumber::plumb(dir = getwd())$run()
->>>>>>> 8c974a9be7fd078dac5bbadcf833bf9446151523
+
--- entrypoint.R
+++ entrypoint.R
@@ -3,28 +3,21 @@ asd <- 12123
 pool <- UNOSODBC::conn_pool("MODBSAS")
 
 # entrypoint.R
-<<<<<<< HEAD
-=======
 #con <- DBI::dbConnect(RSQLite::SQLite(), dbname = ":memory:")
 con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "table.sqlite")
 DBI::dbWriteTable(con, "mtcars", mtcars, overwrite=TRUE)
 
 pool <- pool::dbPool(con)
 
->>>>>>> 8c974a9be7fd078dac5bbadcf833bf9446151523
+
 pr <- plumb("plumber.R")
 
 # Register an exit handler
 pr$registerHook("exit", function(){
     print("See ya later")
-<<<<<<< HEAD
-    print(asd)
-    print(pool)
-    
-=======
     testthat::expect_equal(pool::poolClose(pool),NULL)
     testthat::expect_equal(DBI::dbDisconnect(con),TRUE)
->>>>>>> 8c974a9be7fd078dac5bbadcf833bf9446151523
+
 })
 
 pr
