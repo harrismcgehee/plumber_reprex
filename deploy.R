77bfb08e600e21787b2cf1b4b3d54800cf685895
Harrison McGehee <harrison.mcgehee@unos.org>
2019-02-28 15:04:08 -0500

    make more reproducible

--- deploy.R
+++ deploy.R
@@ -1,4 +1,13 @@
 
+# install.packages("tidyverse")
+# install.packages("plumber")
+# install.packages("swagger")
+# install.packages("RSQLite")
+# install.packages("pool")
+
+con <- DBI::dbConnect(RSQLite::SQLite(), dbname = "table.sqlite")
+DBI::dbWriteTable(con, "mtcars", mtcars, overwrite=TRUE)
+DBI::dbDisconnect(con)
+
 library(plumber)
 plumber::plumb(dir = getwd())$run()
-
